7f76a1745bd35dcab47afa60482abe18af729374
Samuel Streeter <38993697+ssstree@users.noreply.github.com>
2022-03-03 11:06:51 -0500

    Add files via upload

---
+++ Code_for_Lessons/Lesson5_SetRTC/Lesson5_SetRTC.ino
@@ -0,0 +1,92 @@
+// S. Streeter
+// February 2022
+//
+// Date and time functions using a DS3231 RTC connected via I2C and Wire lib.
+
+//----------------------------------------------------------------------------------------
+
+#include "RTClib.h"
+
+// Create RTC variable
+RTC_DS3231 rtc;
+
+// Set the current date and time for the RTC
+// Note: This does not have to be perfect!
+int SetYear  = 2022;
+int SetMonth = 3;
+int SetDay   = 1;
+int SetHour  = 20;
+int SetMin   = 9;
+int SetSec   = 30;
+
+//----------------------------------------------------------------------------------------
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+void setup () {
+  Serial.begin(57600);
+
+#ifndef ESP8266
+  while (!Serial); // Wait for serial port to connect. Needed for native USB
+#endif
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
+
+  // Set the time of the RTC
+  // Year, month, day, hour (24), minute, second
+  rtc.adjust(DateTime(SetYear, SetMonth, SetDay, SetHour, SetMin, SetSec));
+  
+}
+
+void loop () {
+    
+    DateTime now = rtc.now();
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+
+    Serial.print("Seconds since midnight 1/1/1970 (Unix time) = ");
+    Serial.print(now.unixtime());
+    Serial.print("s = ");
+    Serial.print(now.unixtime() / 86400L);
+    Serial.println("d");
+
+    // Calculate a date which is 7 days, 12 hours, 30 minutes, 6 seconds into the future
+    DateTime future (now + TimeSpan(7,12,30,6));
+
+    Serial.print("Now + 7d + 12h + 30m + 6s: ");
+    Serial.print(future.year(), DEC);
+    Serial.print('/');
+    Serial.print(future.month(), DEC);
+    Serial.print('/');
+    Serial.print(future.day(), DEC);
+    Serial.print(' ');
+    Serial.print(future.hour(), DEC);
+    Serial.print(':');
+    Serial.print(future.minute(), DEC);
+    Serial.print(':');
+    Serial.print(future.second(), DEC);
+    Serial.println();
+
+    Serial.print("Temperature: ");
+    Serial.print(rtc.getTemperature());
+    Serial.println(" C");
+
+    Serial.println();
+    delay(3000);
+}
